1d13681e22562f04a243c56f819daef6f8fc8b44
Gonzalo <gonzalo.castelli@hotmail.com>
2014-05-24 18:36:35 -0700

    Libreria de Matrices

---
+++ matlib.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*Authors: Grupo 1: Castelli, Diaz, Viotti Bozzini*/
+
+/*Libreria para manejo de matrices. Inlcuye funciones para:
+
+	Crear matriz
+	Inicializar matriz
+	Inicializar matriz en random
+
+	Obtener cantidad de filas y/o columnas de una matriz
+	Valor maximo
+	Valor Minimo
+
+	Sumar matrices
+	Restar matrices
+	Multiplicar matrices
+	Calcular determinante
+
+*/
+
+
+
+
+
+
