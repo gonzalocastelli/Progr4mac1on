cc818533bd168b9155bd0ee2227935e8d5ce80c2
Gonzalo <gonzalo.castelli@hotmail.com>
2014-05-24 20:45:37 -0700

    Libreria matrices

--- matlib.h
+++ matlib.h
@@ -24,16 +24,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <alloc.h>
+
+#include <time.h>
 
 #ifndef MATLIB_H
 
 	#define MATLIB_H
 
-	char *create_matrix (int, int, char);
 
-#endif
+/*Prototipos de funciones*/
 
+char *create_matrix (int, int, char);
+char *create_crazy_matrix (int, int, char, char);
+
+char randomize (char, char);
 
 
 
@@ -60,3 +64,54 @@ char *create_matrix (int N, int M, char initialval)
 
 }
 
+/*Funcion que crea una matriz aleatoria de N filas, M columnas*/
+/*Devuelve un puntero al inicio de la matriz*/
+/*El tipo de elementos de la matriz es char*/
+
+char *create_crazy_matrix (int M, int N, char minval, char maxval)
+{
+
+	char *matrix = NULL;
+
+	if ((matrix = (char *) malloc (M*N)) == NULL)
+	{
+		printf ("Not enough memory to allocate matrix\n");	/*No memory*/
+		exit (1);
+	}
+
+	if (maxval <= minval)
+	{
+
+		printf ("Minimum value is higher than maximum\n");	/*Error message*/
+		exit (1);
+	}
+
+
+	int i=0;
+	int randomnumber = 0;
+
+	for (i=0 ; i<(M*N) ; i++, randomnumber = randomize(minval, maxval))
+	  *(matrix+i) = randomnumber;
+
+}
+
+
+/*Funcion anexa que crea numeros aleatorios*/
+/*RECIBE: valor minimo y maximo*/
+/*DEVUELVE: Numero aleatorio comprenido entre el minimo y maximo*/
+
+char randomize (char minval, char maxval)
+{
+
+	int randomnumber=0;
+
+	srand (time(NULL));	/*Seed*/
+
+	randomnumber = ( (rand() % (maxval-minval)) + minval);	/*Randomize according to minval and maxval*/
+
+	return randomnumber;
+
+}
+
+
+#endif
