c5c489e6059496f928138a06a3a4862c8928b4a5
Gonzalo <gonzalo.castelli@hotmail.com>
2014-05-24 18:54:24 -0700

    Libreria matriz

--- matlib.h
+++ matlib.h
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+
+/*Libreria para manejo de matrices*/
 
 /*Authors: Grupo 1: Castelli, Diaz, Viotti Bozzini*/
 
@@ -22,7 +21,42 @@
 */
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <alloc.h>
+
+#ifndef MATLIB_H
+
+	#define MATLIB_H
+
+	char *create_matrix (int, int, char);
+
+#endif
 
 
 
+
+/*Funcion que crea una matriz de N filas, M columnas, con sus celdas inicializadas en el valor initval*/
+/*Devuelve un puntero al inicio de la matriz*/
+/*El tipo de elementos de la matriz es char*/
+
+char *create_matrix (int N, int M, char initialval)
+{
+
+	char *matrix = NULL;
+
+	if( (matrix = (char *) malloc (N*M)) == NULL)
+	{
+		printf ("Not enough memory to allocate matrix\n");	/*Exit program if not enough mem*/
+		exit (1);
+	}
+
+	int i=0;
+
+	for (i=0 ; i<(M*N) ; i++)
+
+	  *(matrix+i) = initialval;			/*Initializes matrix with selected number*/
+
+}
 
